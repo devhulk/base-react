aa66edf02ac4b1da2725881c94aed5d908c6328b
devhulk <gerald.yerden@icloud.com>
2018-01-23 09:43:38 -0500

    mods

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import RaisedButton from 'material-ui/RaisedButton';
+import RaisedButton from 'material-ui/Button';
+import AppBar from 'material-ui/AppBar';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme'
 
 class Welcome extends React.Component {
   constructor(props) {
@@ -9,13 +13,41 @@ class Welcome extends React.Component {
     this.handleChange = this.handleChange.bind(this);
     this.state = {name: 'Medium'};
   }
+
+
   handleChange(e) {
     this.setState({name: e.target.value});
   }
   render() {
+  let cardStyle = {
+    height : "400px",
+    width : "33%",
+    margin : "10px",
+    marginLeft: "0px"
+  }
+  let appBarStyle = {
+    color : "dddddd"
+  }
     return (
-      <MuiThemeProvider>
-      <RaisedButton label="Default" />
+      // Replace MuiTheme with Grid
+      <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+        <AppBar
+    title="Superbowl Articles"
+    iconClassNameRight="muidocs-icon-navigation-expand-more"
+    style={appBarStyle}
+  />
+      <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+        <Card style={cardStyle}>
+          <CardHeader
+          title="Super Bowl Champs" 
+          />
+          <CardMedia
+          overlay={<CardTitle title="How'd They do it?" subtitle="click for details..." />}
+        >
+          <img src="/eaglebitties.jpg" alt="" />
+        </CardMedia>
+        </Card>
+      </MuiThemeProvider>
       </MuiThemeProvider>
     );
   }
